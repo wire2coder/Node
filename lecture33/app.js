d4be0b4cbca69a53f1c981a2ac8aa35f3b51766d
CNNENGRING for tape bar coders <cnnicentral@turner.com>
2017-08-20 02:32:14 -0400

    Used Node built-in event library and my own config.js file

--- lecture33/app.js
+++ lecture33/app.js
@@ -1,14 +1,18 @@
-var Emitter = require('./emitter.js')
+// using Node events library
+var Emitter = require('events');
+
+var eventConfig = require('./config.js');
 
 var emit1 = new Emitter();
 
-emit1.on('greet', function() {
+emit1.on(eventConfig.events.GREET, function() {
     console.log('Greeting from a ')
 })
 
-emit1.on('greet', function() {
-    console.log('give me a good')
+emit1.on(eventConfig.events.GREET, function() {
+    console.log('give me a dddd')
 })
 
 // emit1.events.prop1[0]();
-emit1.emit('greet');
+emit1.emit(eventConfig.events.GREET);
+
