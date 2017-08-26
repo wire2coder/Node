e89eaf6e750fb7db89bb19c2feafc45dc1372de9
Andy <x64amd@outlook.com>
2017-08-25 20:43:33 -0400

    Added express.static/middleware to serve the style.css file

---
+++ lecture75/app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+
+// ??? express is a constructor function
+// run the function to get new express object
+// make a pointer 'app' and point it
+// to the object.
+var app = express();
+
+var port = process.env.port || 3000;
+
+// now use express as a middleware
+app.use('/assets', express.static(__dirname + '/pub'));
+
+// use express to handle routing
+// for the server
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/json', function(req, res) {
+    res.json({name1: 'value1', name2: 'value2'})
+})
+
+app.get('/info/:id1', function(req, res) {
+    // :id variable is added to the 
+    // req.params object as property
+    res.send(`<html><head></head><body><h4>${req.params.id1}</h4></body></html>`)
+})
+
+app.listen(port, function() {
+    console.log(`Express server running on port ${port}`)
+})
